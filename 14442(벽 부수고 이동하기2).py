1e28822b440baf07405ec7d3bef39e43d73e7701
이주언 <wndjs8225@naver.com>
2024-04-09 15:49:27 +0900

    20240409

---
+++ 14442(벽 부수고 이동하기2).py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, M, K = map(int, input().split())
+arr = [list(map(int, input().rstrip())) for _ in range(N)]
+visit = [[[0] * (K+1) for _ in range(M)] for _ in range(N)]
+
+queue = deque()
+queue.append((0, 0, 0))
+visit[0][0][0] = 1
+idx = -1
+while queue:
+    x, y, c = queue.popleft()
+
+    if x == N-1 and y == M-1:
+        idx = c
+        break
+
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < N and 0 <= ny < M:
+            if arr[nx][ny] and c+1 < (K+1) and not visit[nx][ny][c+1]:
+                visit[nx][ny][c+1] = visit[x][y][c] + 1
+                queue.append((nx, ny, c+1))
+            elif not arr[nx][ny] and not visit[nx][ny][c]:
+                visit[nx][ny][c] = visit[x][y][c] + 1
+                queue.append((nx, ny, c))
+
+for line in visit:
+    print(line)
+
+# ans = N * M +1
+# for i in range(K+1):
+#     if visit[N-1][M-1][i]:
+#        if ans > visit[N-1][M-1][i]:
+#            ans = visit[N - 1][M - 1][i]
+# if ans == N * M +1:
+#     ans = -1
+if c == -1:
+    ans = c
+else:
+    ans = visit[-1][-1][c]
+print(ans)
